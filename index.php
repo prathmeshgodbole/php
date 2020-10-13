d3a7b569618629e3e12a7ed75863d19a6b8ee11c
prathmeshgodbole <godbole611@gmail.com>
2020-10-13 13:03:45 +0530

    Connect-Get_Data_From_Db & Render_Data_to_broweser

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ if(!$conn){
 }
 
 //Write Query for all type pizzas
-$sql='SELECT id,title,ingredients FROM pizzas';
+// $sql='SELECT id,title,ingredients FROM pizzas';
+$sql='SELECT id,title,ingredients,email FROM pizzas ORDER BY created_at';
 
 //Make Query & get result
 $result=mysqli_query($conn,$sql);
@@ -24,7 +25,7 @@ mysqli_free_result($result);
 //close_connection
 mysqli_close($conn);
 
-print_r($pizzas);
+// print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
@@ -32,7 +33,25 @@ print_r($pizzas);
 
 <?php include('template/header.php');?>
 
+<h4 class="center grey-text">Pizzas..!</h4>
+    <div class="container">
+        <div class="row">
+                <?php foreach($pizzas as $pizza){?> 
+                         <div class="col s6 md3">
+                             <div class="card z-depth-0">
+                                  <div class="card-content center">
+                                      <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                                      <div><?php echo htmlspecialchars($pizza['ingredients']);?></div>
+                                      <div><?php echo htmlspecialchars($pizza['email']);?></div>
+                                  </div>
+                                 <div class="card-action center-align">
+                                    <a class="brand-text" href="#">More_Info</a>
+                                 </div>
+                            </div>
+                         </div>
+                <?php } ?>
+        </div>
+    </div>
+
 <?php include('template/footer.php');?>
-
-
 </html>
